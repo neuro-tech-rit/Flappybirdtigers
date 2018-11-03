e90e66ed1664f8f9a0bb678ca877d6af0384ada1
ahh1539 <31779206+ahh1539@users.noreply.github.com>
2018-11-03 18:07:52 -0400

    Flappy bird code

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <Mouse.h>
+//@author Hillary
+int min = 0;
+int max = 0;
+
+void setup() {
+    // put your setup code here, to run once:
+    Serial.begin(9600);
+    pinMode(A5, INPUT);
+    Mouse.begin();
+}
+
+void loop() {
+    int x = analogRead(A5);
+    if(x < min){
+        min = x;
+    }
+    if(x > max){
+        max = x;
+    }
+    x = map(x, min, max, 0, 100);
+    if (x > 30)
+    {
+        Mouse.click(MOUSE_LEFT);
+    }
+    Serial.println(x);
+    delay(50);
+}
